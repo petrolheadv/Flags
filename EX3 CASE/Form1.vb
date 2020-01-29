74dcd6c09bfa1f9e8788c8478ac6a51ac83fb42e
petrolheadv <35179383+petrolheadv@users.noreply.github.com>
2020-01-29 19:29:21 +0000

    Add project files.

---
+++ EX3 CASE/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim itemselect As String
+        itemselect = ListBox1.SelectedItem
+
+        Select Case itemselect
+            Case "England"
+                Label1.Text = itemselect
+                Label2.Text = "Hello Programmer"
+                PictureBox1.Image = My.Resources.enn
+
+            Case "Germany"
+                Label1.Text = itemselect
+                Label2.Text = "Hallo Programmierer"
+                PictureBox1.Image = My.Resources.german
+
+            Case "Mexico"
+                Label1.Text = itemselect
+                Label2.Text = "Hola Programador"
+                PictureBox1.Image = My.Resources.mexico
+
+            Case "Italy"
+                Label1.Text = itemselect
+                Label2.Text = "Ciao Programmator"
+                PictureBox1.Image = My.Resources.italy
+        End Select
+    End Sub
+End Class
---
+++ EX3 CASE/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EX3 CASE")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EX3 CASE")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("81fb4d9f-d422-4d2f-be0d-6ca2b6a986d0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
